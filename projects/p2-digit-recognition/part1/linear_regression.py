57229c9a01ff7aa33fb2360ab2cb534137ec9a44
Alex Rohrbach <alex.rohrbach@berkeley.edu>
2025-03-08 19:42:34 -0800

    Finish closed linear regression solution

--- projects/p2-digit-recognition/part1/linear_regression.py
+++ projects/p2-digit-recognition/part1/linear_regression.py
@@ -15,8 +15,10 @@ def closed_form(X, Y, lambda_factor):
         theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
         represents the y-axis intercept of the model and therefore X[0] = 1
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    d = X.shape[1] # of features (including bias)
+
+    theta = np.linalg.inv(X.T @ X + lambda_factor * np.identity(d)) @ X.T @ Y
+    return theta
 
 ### Functions which are already complete, for you to use ###
 
