d3cd071fb5d13598d75509df408e32a909d0dca4
Alex Rohrbach <alex.rohrbach@berkeley.edu>
2025-03-09 17:06:03 -0700

    Add svm functions

--- projects/p2-digit-recognition/part1/svm.py
+++ projects/p2-digit-recognition/part1/svm.py
@@ -15,7 +15,13 @@ def one_vs_rest_svm(train_x, train_y, test_x):
     Returns:
         pred_test_y - (m,) NumPy array containing the labels (0 or 1) for each test data point
     """
-    raise NotImplementedError
+    model = LinearSVC(random_state=0, C=.1)
+
+    model.fit(train_x, train_y)
+
+    pred_test_y = model.predict(test_x)
+    
+    return pred_test_y
 
 
 def multi_class_svm(train_x, train_y, test_x):
@@ -29,7 +35,13 @@ def multi_class_svm(train_x, train_y, test_x):
     Returns:
         pred_test_y - (m,) NumPy array containing the labels (int) for each test data point
     """
-    raise NotImplementedError
+    model = LinearSVC(multi_class='ovr', random_state=0, C=.1)
+
+    model.fit(train_x, train_y)
+
+    pred_test_y = model.predict(test_x)
+
+    return pred_test_y
 
 
 def compute_test_error_svm(test_y, pred_test_y):
