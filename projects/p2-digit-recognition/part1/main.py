36dba9e7e345a3ab3c0cdbce10fe7c9275969fc0
Alex Rohrbach <alex.rohrbach@berkeley.edu>
2025-03-09 20:47:44 -0700

    Change comments for running functions

--- projects/p2-digit-recognition/part1/main.py
+++ projects/p2-digit-recognition/part1/main.py
@@ -23,33 +23,28 @@ train_x, train_y, test_x, test_y = get_MNIST_data()
 # 2. Linear Regression with Closed Form Solution
 #######################################################################
 
-# TODO: first fill out functions in linear_regression.py, otherwise the functions below will not work
+# def run_linear_regression_on_MNIST(lambda_factor=0.01):
+#     """
+#     Trains linear regression, classifies test data, computes test error on test set
+
+#     Returns:
+#         Final test error
+#     """
+#     train_x, train_y, test_x, test_y = get_MNIST_data()
+#     train_x_bias = np.hstack([np.ones([train_x.shape[0], 1]), train_x])
+#     test_x_bias = np.hstack([np.ones([test_x.shape[0], 1]), test_x])
+#     theta = closed_form(train_x_bias, train_y, lambda_factor)
+#     test_error = compute_test_error_linear(test_x_bias, test_y, theta)
+#     return test_error
 
 
-def run_linear_regression_on_MNIST(lambda_factor=0.01):
-    """
-    Trains linear regression, classifies test data, computes test error on test set
-
-    Returns:
-        Final test error
-    """
-    train_x, train_y, test_x, test_y = get_MNIST_data()
-    train_x_bias = np.hstack([np.ones([train_x.shape[0], 1]), train_x])
-    test_x_bias = np.hstack([np.ones([test_x.shape[0], 1]), test_x])
-    theta = closed_form(train_x_bias, train_y, lambda_factor)
-    test_error = compute_test_error_linear(test_x_bias, test_y, theta)
-    return test_error
+# # Don't run this until the relevant functions in linear_regression.py have been fully implemented.
+# print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=1))
 
 
-# Don't run this until the relevant functions in linear_regression.py have been fully implemented.
-print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=1))
-
-
-# #######################################################################
-# # 3. Support Vector Machine
-# #######################################################################
-
-# # TODO: first fill out functions in svm.py, or the functions below will not work
+#######################################################################
+# 3. Support Vector Machine
+#######################################################################
 
 # def run_svm_one_vs_rest_on_MNIST():
 #     """
@@ -84,44 +79,44 @@ print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_fa
 
 # print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
 
-# #######################################################################
-# # 4. Multinomial (Softmax) Regression and Gradient Descent
-# #######################################################################
+#######################################################################
+# 4. Multinomial (Softmax) Regression and Gradient Descent
+#######################################################################
 
-# # TODO: first fill out functions in softmax.py, or run_softmax_on_MNIST will not work
-
-
-# def run_softmax_on_MNIST(temp_parameter=1):
-#     """
-#     Trains softmax, classifies test data, computes test error, and plots cost function
-
-#     Runs softmax_regression on the MNIST training set and computes the test error using
-#     the test set. It uses the following values for parameters:
-#     alpha = 0.3
-#     lambda = 1e-4
-#     num_iterations = 150
-
-#     Saves the final theta to ./theta.pkl.gz
-
-#     Returns:
-#         Final test error
-#     """
-#     train_x, train_y, test_x, test_y = get_MNIST_data()
-#     theta, cost_function_history = softmax_regression(train_x, train_y, temp_parameter, alpha=0.3, lambda_factor=1.0e-4, k=10, num_iterations=150)
-#     plot_cost_function_over_time(cost_function_history)
-#     test_error = compute_test_error(test_x, test_y, theta, temp_parameter)
-#     # Save the model parameters theta obtained from calling softmax_regression to disk.
-#     write_pickle_data(theta, "./theta.pkl.gz")
-
-#     # TODO: add your code here for the "Using the Current Model" question in tab 6.
-#     #      and print the test_error_mod3
-#     return test_error
+# TODO: first fill out functions in softmax.py, or run_softmax_on_MNIST will not work
 
 
-# print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1))
+def run_softmax_on_MNIST(temp_parameter=1):
+    """
+    Trains softmax, classifies test data, computes test error, and plots cost function
 
-# # TODO: Find the error rate for temp_parameter = [.5, 1.0, 2.0]
-# #      Remember to return the tempParameter to 1, and re-run run_softmax_on_MNIST
+    Runs softmax_regression on the MNIST training set and computes the test error using
+    the test set. It uses the following values for parameters:
+    alpha = 0.3
+    lambda = 1e-4
+    num_iterations = 150
+
+    Saves the final theta to ./theta.pkl.gz
+
+    Returns:
+        Final test error
+    """
+    train_x, train_y, test_x, test_y = get_MNIST_data()
+    theta, cost_function_history = softmax_regression(train_x, train_y, temp_parameter, alpha=0.3, lambda_factor=1.0e-4, k=10, num_iterations=150)
+    plot_cost_function_over_time(cost_function_history)
+    test_error = compute_test_error(test_x, test_y, theta, temp_parameter)
+    # Save the model parameters theta obtained from calling softmax_regression to disk.
+    write_pickle_data(theta, "./theta.pkl.gz")
+
+    # TODO: add your code here for the "Using the Current Model" question in tab 6.
+    #      and print the test_error_mod3
+    return test_error
+
+
+print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1))
+
+# TODO: Find the error rate for temp_parameter = [.5, 1.0, 2.0]
+#      Remember to return the tempParameter to 1, and re-run run_softmax_on_MNIST
 
 # #######################################################################
 # # 6. Changing Labels
