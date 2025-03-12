77a64bea4da289fdd4019241c4ead16a007abeb5
Alex Rohrbach <alex.rohrbach@berkeley.edu>
2025-03-11 19:33:23 -0700

    Add mod 3 version of test error

--- projects/p2-digit-recognition/part1/softmax.py
+++ projects/p2-digit-recognition/part1/softmax.py
@@ -130,8 +130,10 @@ def update_y(train_y, test_y):
         test_y_mod3 - (n, ) NumPy array containing the new labels (a number between 0-2)
                     for each datapoint in the test set
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+    train_y_mod3 = train_y % 3
+    test_y_mod3 = test_y % 3
+
+    return train_y_mod3, test_y_mod3
 
 def compute_test_error_mod3(X, Y, theta, temp_parameter):
     """
@@ -148,8 +150,14 @@ def compute_test_error_mod3(X, Y, theta, temp_parameter):
     Returns:
         test_error - the error rate of the classifier (scalar)
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+    predicted_labels = get_classification(X, theta, temp_parameter)
+
+    predicted_labels_mod3 = predicted_labels % 3
+
+    error_count = np.sum(predicted_labels_mod3 != Y)
+    test_error = error_count / len(Y)
+
+    return test_error
 
 def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterations):
     """
